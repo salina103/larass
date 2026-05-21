ad5832a261ca122fda2ddd8f1b7d3d5dfb3afe54
salina103 <ututulvtutu@gmail.com>
2026-05-21 22:30:22 +0500

    12 работа

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -153,6 +153,11 @@
                         <i class="fas fa-th-large"></i>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}" role="button">
+                        <i class="fas fa-sign-out-alt"></i> Выход
+                    </a>
+                </li>
             </ul>
         </nav>
         <!-- /.navbar -->
@@ -160,9 +165,9 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="{{ route('admin.index') }}" class="brand-link">
+            <a href="{{ route('home') }}" class="brand-link">
                 <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">На сайт</span>
             </a>
 
             <!-- Sidebar -->
@@ -173,7 +178,7 @@
                         <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
                     </div>
                 </div>
 
@@ -212,13 +217,13 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
                                     <a href="{{ route('categories.index') }}" class="nav-link">
-                                        <i class="far fa-list-alt  nav-icon"></i>
+                                        <i class="far fa-circle  nav-icon"></i>
                                         <p>Список категорий</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
                                     <a href="{{ route('categories.create') }}" class="nav-link">
-                                        <i class="far fa-edit nav-icon"></i>
+                                        <i class="far fa-circle nav-icon"></i>
                                         <p>Новая категория</p>
                                     </a>
                                 </li>
@@ -236,13 +241,13 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
                                     <a href="{{ route('tags.index') }}" class="nav-link">
-                                        <i class="far fa-list-alt  nav-icon"></i>
+                                        <i class="far fa-circle  nav-icon"></i>
                                         <p>Список тегов</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
                                     <a href="{{ route('tags.create') }}" class="nav-link">
-                                        <i class="far fa-edit nav-icon"></i>
+                                        <i class="far fa-circle nav-icon"></i>
                                         <p>Новый тег</p>
                                     </a>
                                 </li>
@@ -261,44 +266,14 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
                                     <a href="{{ route('posts.index') }}" class="nav-link">
-                                        <i class="far fa-list-alt nav-icon"></i>
+                                        <i class="far fa-circle nav-icon"></i>
                                         <p>Список статей</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
                                     <a href="{{ route('posts.create') }}" class="nav-link">
-                                        <i class="far fa-edit nav-icon"></i>
+                                        <i class="far fa-circle nav-icon"></i>
                                         <p>Новая статья</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
-                                <p>
-                                    Dashboard
-                                    <i class="right fas fa-angle-left"></i>
-                                </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="../../index.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Dashboard v1</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="../../index2.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Dashboard v2</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="../../index3.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Dashboard v3</p>
                                     </a>
                                 </li>
                             </ul>
